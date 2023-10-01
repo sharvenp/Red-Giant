2c4fe77adbc36dfb3fcc52b8afa7814c8ad7426b
sharvenp <sharvenprasad101@gmail.com>
2023-10-01 00:22:03 -0400

    add spaceship movement and 3rd person camera

--- Red-Giant/Assets/Scripts/Spaceship/Player.cs
+++ Red-Giant/Assets/Scripts/Spaceship/Player.cs
@@ -5,28 +5,48 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Player : MonoBehaviour
 {
-    public float forwardSpeed = 25f;
-    public float strafeSpeed = 7.5f;
-    public float hoverSpeed = 5f;
+    public float pitchPower, rollPower, yawPower, enginePower;
 
-    private float activeForwardSpeed;
-    private float activeStrafeSpeed;
-    private float activeHoverSpeed;
+    public float rollAcceleration = 2f;
+    public float pitchAcceleration = 2f;
+    public float yawAcceleration = 2f;
 
-    private float forwardAcceleration = 2.5f;
-    private float strafeAcceleration = 2f;
-    private float hoverAcceleration = 2f;
+    private float activeRoll, activePitch, activeYaw;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
+        float engine = 0f;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            engine = enginePower;
+        }
+        
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            engine = -enginePower;
+        }
+
+        rb.AddForce(transform.forward * engine * Time.deltaTime);
+
         float vertical = Input.GetAxisRaw("Vertical");
-        float horizontal = Input.GetAxisRaw("Horizontal"); 
-        float hover = Input.GetAxisRaw("Hover");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float yaw = Input.GetAxisRaw("Yaw");
 
-        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, vertical * forwardSpeed, forwardAcceleration * Time.deltaTime);
-        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, horizontal * strafeSpeed, strafeAcceleration * Time.deltaTime);
-        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, hover * hoverSpeed, hoverAcceleration * Time.deltaTime);
+        activePitch = Mathf.Lerp(activePitch, vertical * pitchPower, pitchAcceleration * Time.deltaTime);
+        activeRoll = Mathf.Lerp(activeRoll, horizontal * rollPower, rollAcceleration * Time.deltaTime);
+        activeYaw = Mathf.Lerp(activeYaw, yaw * yawPower, yawAcceleration * Time.deltaTime);
 
-        transform.position += (transform.forward * activeForwardSpeed + transform.right * activeStrafeSpeed + transform.up * activeHoverSpeed ) * Time.deltaTime;
+        transform.Rotate(activePitch * pitchPower * Time.deltaTime, 
+                            activeYaw * yawPower * Time.deltaTime,
+                            -activeRoll * rollPower * Time.deltaTime);
+
     }
 }
