0e2fc6a884a7e4e9d6736fd50a05159a7f8976ed
life-comes-forth <50253842+life-comes-forth@users.noreply.github.com>
2023-10-01 14:37:46 -0400

    gravity script complete

--- Red-Giant/Assets/Scripts/Gravity.cs
+++ Red-Giant/Assets/Scripts/Gravity.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gravity : MonoBehaviour
+public class Attractor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody rb;
+    const float G = 999.5f;
+
+    public static List<Attractor> Attractors;
+
+    private void FixedUpdate()
     {
-        
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+            Attract(attractor);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        if (Attractors == null)
+            Attractors = new List<Attractor>();
+        Attractors.Add(this);
+
     }
+
+    void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+
+    void Attract (Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance == 0f)
+            return;
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+
 }
