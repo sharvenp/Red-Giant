1b24c5b7bacf348734c6701962ebe3f1962903b6
sharvenp <sharvenprasad101@gmail.com>
2023-10-01 20:24:04 -0400

    add thruster sound fx

--- Red-Giant/Assets/Scripts/Spaceship/Player.cs
+++ Red-Giant/Assets/Scripts/Spaceship/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
     public ParticleSystem[] backExhausts;
     public ParticleSystem[] frontExhausts;
 
+    public AudioSource thursterAudio;
+
     private float activeRoll, activePitch, activeYaw;
     Rigidbody rb;
 
@@ -31,9 +33,11 @@ public class Player : MonoBehaviour
             if (isOn && !exhaust.isPlaying)
             {
                 exhaust.Play();
+                thursterAudio.Play();
             }
             else if (!isOn && exhaust.isPlaying)
             {
+                thursterAudio.Pause();
                 exhaust.Stop();
             }
         }
