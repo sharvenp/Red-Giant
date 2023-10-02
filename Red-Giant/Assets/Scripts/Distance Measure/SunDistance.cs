dcc7b06a04fabade4efa1c3a8ac4fb1d69eb75b2
sharvenp <sharvenprasad101@gmail.com>
2023-10-02 15:39:18 -0400

    add sun distance script

---
+++ Red-Giant/Assets/Scripts/Distance Measure/SunDistance.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SunDistance : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform sun;
+
+    public LayerMask sunMask;
+
+    public TextMeshProUGUI distanceText;
+
+    private void Update()
+    {
+        if(Physics.Linecast(player.position, sun.position, out RaycastHit hit, sunMask))
+        {
+            float distance = hit.distance;
+            string distString = distance.ToString("0.00");
+            distanceText.text = $"{distString} KM";
+        }
+    }
+
+}
